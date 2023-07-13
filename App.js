74168dbf80d1761c8f0791ec9f6fbfc6f59138f3
Paulo Sergio Dias <paulosvdd05@gmail.com>
2023-07-13 10:07:49 -0300

    criando função para criar tabela

--- App.js
+++ App.js
@@ -4,20 +4,48 @@ import { openDatabase } from 'react-native-sqlite-storage';
 
 var db = openDatabase({ name: 'UserDatabase.db' });
 
-
+const initialState = {
+  nome: '',
+  desc: ''
+}
 
 export default class App extends Component {
+
+  state = {
+    ...initialState
+  }
+
+  criarTabela = () => {
+    db.transaction(function (tx) {
+      tx.executeSql(
+        'CREATE TABLE IF NOT EXISTS Produtos (prod_id INT AUTO_INCREMENT, prod_nome varchar(30), prod_desc varchar(70), PRIMARY KEY (prod_id))',  //Query to execute as prepared statement
+        [],  //Argument to pass for the prepared statement
+        (tx, results) => {
+          console.warn(tx + '\n' + results);
+        }  //Callback function to handle the result
+      );
+    });
+  }
+
+  inserirProduto = () => {
+
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.texto1}>Cadastrar Produto</Text>
         <View>
           <TextInput style={styles.input}
-            placeholder="Nome do Produto..." />
+            placeholder="Nome do Produto..."
+            value={this.state.nome}
+            onChangeText={nome => this.setState({ nome })} />
           <TextInput style={styles.input}
-            placeholder="Descrição do Produto..." />
+            placeholder="Descrição do Produto..."
+            value={this.state.desc}
+            onChangeText={nome => this.setState({ desc })} />
         </View>
-        <TouchableNativeFeedback>
+        <TouchableNativeFeedback onPress={this.criarTabela}>
           <View style={styles.botao}>
             <Text style={styles.texto2}>Cadastrar</Text>
           </View>
@@ -39,10 +67,10 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#fff'
   },
-  texto2:{
-    fontSize:15,
-    color:'#171717',
-    fontWeight:'bold'
+  texto2: {
+    fontSize: 15,
+    color: '#171717',
+    fontWeight: 'bold'
   },
   input: {
     height: 40,
@@ -59,7 +87,7 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     marginTop: 10,
     backgroundColor: '#fff',
-    padding:15,
-    borderRadius:10
+    padding: 15,
+    borderRadius: 10
   }
 })
